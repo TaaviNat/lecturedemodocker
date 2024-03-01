5ef55906f38d3dac1e62cefb54ba3393fb6c23dc
TaaviNat <taavi.natynki@gmail.com>
2024-03-01 13:44:26 +0200

    second

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,11 @@
 package org.example;
+public class App {
+    public int addMe(int a, int b) {
+        return a + b;
+    }
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println(app.addMe(2, 3));
     }
 }
